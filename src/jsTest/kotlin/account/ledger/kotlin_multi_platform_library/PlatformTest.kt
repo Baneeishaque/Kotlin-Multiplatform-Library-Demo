0949ce79bda9c30d6eb7970e9db63c9e0309b3e3
Banee Ishaque K <baneeishaque2@gmail.com>
2022-09-09 04:51:21 +0530

    syntax fix

--- src/jsTest/kotlin/account/ledger/kotlin_multi_platform_library/PlatformTest.kt
+++ src/jsTest/kotlin/account/ledger/kotlin_multi_platform_library/PlatformTest.kt
@@ -1,12 +1,12 @@
-package ndk.ledger.kotlin_multi_platform_library
+package account.ledger.kotlin_multi_platform_library
 
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class PlatformTest {
 
-    // @Test
-    // fun testExample() {
-    //     assertTrue(Greeting().greeting().contains("js"), "Check js is mentioned")
-    // }
+     @Test
+     fun testExample() {
+         assertTrue(Greeting().greeting().contains("js"), "Check js is mentioned")
+     }
 }
